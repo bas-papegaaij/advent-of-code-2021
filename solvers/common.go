a2596224ad534c9ebc881a808d5ed7a540d4ce39
Bas Papegaaij <b.papegaaij@arkoselabs.com>
2021-12-01 16:59:16 +1000

    Remove genericSolver interface.
    This kind of goes against the goal of generalising code between both parts of each challenge

--- solvers/common.go
+++ solvers/common.go
@@ -5,23 +5,8 @@ import (
 	"strconv"
 )
 
-// defined for convenience here so we can
-type genericSolver interface {
-	part1(input []string) (int64, error)
-	part2(input []string) (int64, error)
-}
-
-// convenient where we don't need to do additional input pre-processing
-// e.g. each line from the input is sufficient for the solution functions
-func genericSolve(solver genericSolver, input []string, part int) (int64, error) {
-	switch part {
-	case 1:
-		return solver.part1(input)
-	case 2:
-		return solver.part2(input)
-	}
-
-	return -1, fmt.Errorf("no solution for question part %d", part)
+func invalidPartError(part int) error {
+	return fmt.Errorf("no solution for question part %d", part)
 }
 
 func inputsToInt(input []string) ([]int, error) {
