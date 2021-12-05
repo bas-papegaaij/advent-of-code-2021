01918443b8521d59b54c27df92c2be3879dd2e26
Bas Papegaaij <b.papegaaij@arkoselabs.com>
2021-12-05 13:41:44 +1000

    Add a profile mode for more accurate profiling

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ var solutions []Solver = []Solver{
 var inputFile string
 var questionPart int
 var question int
+var runProfile bool
 
 func main() {
 	parseArgs()
@@ -32,14 +33,47 @@ func main() {
 		panic(err)
 	}
 
+	if runProfile {
+		profileMode(lines)
+	} else {
+		solutionMode(lines)
+	}
+}
+
+func solutionMode(lines []string) {
 	start := time.Now()
 	answer, err := solutions[question].Solve(lines, questionPart)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("solution took", time.Since(start))
-
 	fmt.Println("Answer to question part", questionPart, "was", answer)
+}
+
+func profileMode(lines []string) {
+	runs := make([]time.Duration, 0, 1000)
+	solver := solutions[question]
+	for i := 0; i < 1000; i++ {
+		start := time.Now()
+		solver.Solve(lines, questionPart)
+		runs = append(runs, time.Since(start))
+	}
+
+	var min, max, total time.Duration
+	for i, runtime := range runs {
+		if i == 0 {
+			min = runtime
+		}
+		if runtime < min {
+			min = runtime
+		}
+		if runtime > max {
+			max = runtime
+		}
+		total += runtime
+	}
+	avg := time.Duration(total.Nanoseconds() / int64(len(runs)))
+	fmt.Println("min:", min, "max:", max, "avg:", avg)
 }
 
 func parseLines() ([]string, error) {
@@ -61,6 +95,7 @@ func parseArgs() {
 	flag.StringVar(&inputFile, "input", "input.txt", "Location of the input file, relative to the working directory")
 	flag.IntVar(&question, "question", 0, "Which question to answer")
 	flag.IntVar(&questionPart, "part", 1, "The part of the question to answer - 1 or 2")
+	flag.BoolVar(&runProfile, "prof", false, "Whether to run a profile. If enabled runs the solution 1000 times and grabs an average, min and max runtimes")
 
 	flag.Parse()
 
